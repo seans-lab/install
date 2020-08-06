edc0a195d9bddc8aef2ae1ffd669b91d4773a1d9
Sean <41353165+SeaNGiNX@users.noreply.github.com>
2020-08-07 07:40:34 +1000

    Create install_centos_7.sh

---
+++ vscode/install_centos_7.sh
@@ -0,0 +1,6 @@
+#INSTALLATION OF VS CODE ON CENTOS 7
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+yum check-update
+sudo yum install code
+echo All Done!
