1331ee9fd2786e94d51e37d276c2309ce3a51df3
Sean <41353165+SeaNGiNX@users.noreply.github.com>
2021-02-26 09:45:05 +1100

    Update install_centos7.sh

--- docker/install_centos7.sh
+++ docker/install_centos7.sh
@@ -11,4 +11,6 @@ yum install -y yum-utils
 yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce -y docker-ce-cli -y containerd.io -y                  
+yum install docker-ce -y docker-ce-cli -y containerd.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
