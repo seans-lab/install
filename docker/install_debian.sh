abf4d2120e35226532a0f89eb3c44d6945897cf0
Sean <41353165+seans-lab@users.noreply.github.com>
2023-05-08 12:47:34 +1000

    Create install_debian.sh

---
+++ docker/install_debian.sh
@@ -0,0 +1,15 @@
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+groupadd docker
+usermod -aG docker $USER
+systemctl enable docker.service
+systemctl enable containerd.service
